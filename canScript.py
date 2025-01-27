1c4cbef22bcb2361d0ff69e110d82cbf64cea8ee
Aragya1642 <agoyal1642@gmail.com>
2025-01-27 16:10:48 -0500

    Sets up motor node as 402 device and sends periodic sync messages - needs testing on hardware

--- canScript.py
+++ canScript.py
@@ -1,7 +1,33 @@
 import canopen
+import time
+from canopen.profiles.p402 import BaseNode402
 
 network = canopen.Network()
+network.connect(channel='vcan0', interface='socketcan')
+network.sync.start(0.01) # Transmit sync message every 10 ms
 
-encoder = network.add_node(2, 'eds_files/HTB_FHB_PM_CA_2021-02-24.eds')
-motor = network.add_node(3, 'eds_files/Eds1406C.eds')
+encoder_node = canopen.LocalNode(2, 'eds_files/HTB_FHB_PM_CA_2021-02-24.eds')
+motor_node = BaseNode402(3, 'eds_files/Eds1406C.eds')
 
+network.add_node(encoder_node)
+network.add_node(motor_node)
+
+for node_id in network:
+    print(network[node_id])
+
+# Get the Object Dictionary of a node
+# for obj in encoder_node.object_dictionary.values():
+#     print(f'0x{obj.index:X}: {obj.name}')
+#     if isinstance(obj, canopen.objectdictionary.ODRecord):
+#         for subobj in obj.values():
+#             print(f'  {subobj.subindex}: {subobj.name}')
+
+# device_type = motor_node.sdo[0x1000]
+# print(f"The device type is 0x{device_type.raw:X}")
+
+
+time.sleep(10)
+
+
+network.sync.stop()
+network.disconnect()
