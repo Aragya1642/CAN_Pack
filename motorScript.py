1a1a330b0c8e1f4aa3661326d29f2f5c348d04d8
Aragya1642 <agoyal1642@gmail.com>
2025-02-12 11:09:35 -0500

    Traverses through motor state machine AND spins the motor

--- motorScript.py
+++ motorScript.py
@@ -31,16 +31,25 @@ network.nmt.state = 'PRE-OPERATIONAL'
 motor1.op_mode = 'PROFILED VELOCITY'
 print_system_states(network, motor1)
 
-
 # Network Operational Stage
 network.nmt.state = 'OPERATIONAL'
+motor1.state = 'READY TO SWITCH ON'
+motor1.state = 'SWITCHED ON'
+motor1.state = 'OPERATION ENABLED'
 print_system_states(network, motor1)
+
+# Command the motor to spin by sending a target velocity.
+target_velocity = 80000  # change this value as needed
+print(f"Setting target velocity to {target_velocity}")
+motor1.sdo[0x60FF].raw = target_velocity
 
 try:
     while True:
         pass
 except KeyboardInterrupt:
-    motor1.op_mode = 'NO MODE'
+    motor1.sdo[0x60FF].raw = 0
+    # motor1.op_mode = 'NO MODE'
+    motor1.state = 'SWITCH ON DISABLED'
     print_system_states(network, motor1)
     network.nmt.state = 'INITIALISING'
     print("------------------------------")
